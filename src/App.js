c973b0b195b227e598c498bc6e890ae762c4d187
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-28 20:28:49 -0300

    Create store

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import store from "./store";
+
+function App() {
+    return (
+        <Provider store={store}>
+            <div>
+                <h1>Hello</h1>
+            </div>
+        </Provider>
+    );
+}
+
+export default App;
